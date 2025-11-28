69c782331878ceb000a4ad8a728145c7bc438d26
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-28 10:45:02 +0700

    membuat interface RepositoriSiswa sebagai kontrak repository

---
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,7 @@
+package com.example.roomdatabase.repositori
+
+import com.example.roomdatabase.room.Siswa
+import com.example.roomdatabase.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa{
