9221834d76b2280155e9b674a29860fd8650905c
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-28 10:24:22 +0700

    membuat class DatabaseSiswa sebagai RoomDatabase utama

---
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
