db8ad4b72ba0b233f1e3080ea43fc48df07e231a
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-28 12:58:22 +0700

    Menetapkan route "home" dan titleRes menggunakan R.string.app_name

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
@@ -3,5 +3,6 @@ package com.example.roomdatabase.view.route
 import com.example.roomdatabase.R
 
 object DestinasiHome: DestinasiNavigasi{
-
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
