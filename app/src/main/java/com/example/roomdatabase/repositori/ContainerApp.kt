11876b0b4c08f67b1cbb8d97ee3259c0ab1cbde6
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-28 10:53:12 +0700

    menginisialisasi container dependency pada onCreate aplikasi

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -17,5 +17,10 @@ class ContainerDataApp(private val context : Context):
 }
 
 class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
 
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
