da4048f8da5785d074f04401b868cd61bc596a4d
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-05 09:30:33 +0700

    Tambah initializer DetailViewModel dengan SavedStateHandle dan repositori

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.roomdatabase.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -15,6 +16,10 @@ object PenyediaViewModel{
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(SavedStateHandle = this.createSavedStateHandle(),
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
