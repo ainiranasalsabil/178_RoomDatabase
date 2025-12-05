6c5f6ff29fe6bdd3ac231b003f1c9b7c39cdc147
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-12-05 10:22:57 +0700

    Implementasi StateFlow untuk memuat dan mengamati data siswa secara realtime

--- app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
@@ -18,7 +18,16 @@ class DetailViewModel (
 
     private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
 
-
+    val uiDetailState: StateFlow<DetailSiswaUiState> =
+        repositoriSiswa.getSiswaStream(idSiswa)
+            .filterNotNull()
+            .map {
+                DetailSiswaUiState(detailSiswa = it.toDetailSiswa())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailSiswaUiState()
+            )
 
 /**
  * UI state for ItemDetailsScreen
